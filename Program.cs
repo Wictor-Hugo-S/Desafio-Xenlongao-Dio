c45010971c28b1222c842bbd8a6bfd5451c4f261
Wictor-Hugo-S <75641454+Wictor-Hugo-S@users.noreply.github.com>
2022-02-01 16:20:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+Kogu está buscando as esferas do dragão para invocar Xenlongão e pedir para ele reviver seu amigo Kuriri, que infelizmente morreu de novo na última batalha dos guerreiros Zê.
+Porém Kogu está tendo muita dificuldade para encontrar as esferas, por isso Xenlongão que é seu conhecido há muito tempo, 
+decidiu abrir uma exceção e aceitou ser invocado caso Kogu encontre todas as esferas cujo o número de divisores da quantidade de estrelas da esfera sejam par.
+Por exemplo se existem sete esferas, Kogu não precisaria encontrar as esferas de uma e quatro estrelas, 
+pois elas tem uma quantidade ímpar de divisores, então ele só precisa pegar 5 esferas para invocar Xenlongão.
+
+
+
+
+using System;
 
 namespace Desafio_Dio
 {
