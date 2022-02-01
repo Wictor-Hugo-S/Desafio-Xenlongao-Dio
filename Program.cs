07ee93d581552fd1868b04192d2553d8a6cccead
Wictor-Hugo-S <75641454+Wictor-Hugo-S@users.noreply.github.com>
2022-02-01 16:19:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio_Dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+                int n = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(c - Math.Floor(Math.Sqrt(c)));
+
+            }
+        }
+    }
